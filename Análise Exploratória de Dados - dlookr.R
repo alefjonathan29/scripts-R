591c1039c641e65ae41d86ce7e5defa651233d7e
alefjonathan29 <a.lef93@hotmail.com>
2021-07-30 11:34:49 -0300

    Salvando EAD em pdf

--- Análise Exploratória de Dados - dlookr.R
+++ Análise Exploratória de Dados - dlookr.R
@@ -8,6 +8,14 @@ dados <- iris
 
 #Inserir em "target=" o nome da coluna categórica (ex: a coluna 
 #que está os nomes dos grupos)
+
+#Salvando em html
 dados %>%
   eda_web_report(target = "Species", subtitle = "heartfailure", 
-                 output_dir = "./", output_file = "EDA.html", theme = "blue")
+                 output_dir = "./", output_file = "EDA.html", 
+                 theme = "blue")
+
+#Salvando em pdf
+dlookr::eda_paged_report(iris, subtitle = "Iris", 
+                         output_file = "EDA2.pdf",output_dir = "./",
+                         theme = "blue")
